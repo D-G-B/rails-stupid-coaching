fcd2cec92d0b170b5f03c97f89c856f0b6dba78c
Daniel Brandt <daniel.gordon.brandt@gmail.com>
2021-02-15 16:23:57 +0100

    added dark bootsrap css + link to return to qs

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,6 +3,8 @@ class AnswersController < ApplicationController
     @question = params[:question]
     if @question.downcase == "i am going to work!"
       @answer = "Great!"
+    elsif @question == "what time is it?"
+     @answer = Time.now
     elsif @question.end_with?("?")
       @answer = "Silly question, get dressed and go to work!"
     else
